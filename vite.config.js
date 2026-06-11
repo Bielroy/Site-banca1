37c832173287d9fd6e9971120046fc2f015c2d35
Gabriel Silva Freitas <gabrielsilva8990idh@gmail.com>
2026-06-11 10:57:25 -0300

    Configure Rollup with multiple entry points
    Updated Vite configuration to specify multiple entry points for Rollup.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
@@ -35,6 +36,13 @@ export default defineConfig({
     })
   ],
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    // [AQUI ESTÁ A CORREÇÃO] Avisamos o Vite que existem duas páginas!
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html')
+      }
+    }
   }
 });
